5bb4f7f811816143aea6586f6c91e71cbdebccdf
vrybachenko <vrybache@softserveinc.com>
2023-05-17 17:11:15 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,60 @@
+"use strict";
+
+// var test = "test"
+//
+// let count = 4.6;
+//
+// console.log(4 / 0);
+// console.log('string' - 9);
+//
+// const person = `Bob`
+//
+//
+// // console.log(something)
+//
+// let und;
+// console.log(und)
+//
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmb'];
+// arr[10] = '777';
+// console.log(arr);
+//
+//
+// const obj = {
+//     name: "Josh",
+//     age: 25,
+//     isMarried: false,
+//     1: true
+// }
+//
+// obj.b = '1234';
+// console.log(obj["b"])
+// console.log(obj.b)
+
+//alert('Hello');
+//const result = confirm("Are you here?");
+//console.log(result)
+
+// const answer = prompt("Вам есть 18?", "18");
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Как Ваше имя?', '');
+// answers[1] = prompt('Как Ваша фамилия?', '');
+// answers[2] = prompt('Сколько Вам лет?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
+
+let incr = true,
+    decr = false;
+
+
+
+
+
