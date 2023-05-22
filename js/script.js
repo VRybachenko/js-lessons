53003e4acf4d612c96fdc4b02612ff84bd4faeca
vrybachenko <vrybache@softserveinc.com>
2023-05-22 19:51:15 +0300

    first app on JS

--- js/script.js
+++ js/script.js
@@ -1,58 +1,24 @@
 "use strict";
 
-// var test = "test"
-//
-// let count = 4.6;
-//
-// console.log(4 / 0);
-// console.log('string' - 9);
-//
-// const person = `Bob`
-//
-//
-// // console.log(something)
-//
-// let und;
-// console.log(und)
-//
-// let arr = ['plum.png', 'orange.jpg', 'apple.bmb'];
-// arr[10] = '777';
-// console.log(arr);
-//
-//
-// const obj = {
-//     name: "Josh",
-//     age: 25,
-//     isMarried: false,
-//     1: true
-// }
-//
-// obj.b = '1234';
-// console.log(obj["b"])
-// console.log(obj.b)
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//alert('Hello');
-//const result = confirm("Are you here?");
-//console.log(result)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: {},
+    private: false
+};
 
-// const answer = prompt("Вам есть 18?", "18");
-// const answer = prompt("Вам есть 18?", "18");
-// console.log(answer + 5);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его?', '');
 
-// const answers = [];
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-// answers[0] = prompt('Как Ваше имя?', '');
-// answers[1] = prompt('Как Ваша фамилия?', '');
-// answers[2] = prompt('Сколько Вам лет?', '');
-
-// console.log(typeof(answers));
-// console.log(typeof(null));
-
-// const category = 'toys';
-// console.log(`https://someurl.com/${category}/5`);
-
-let incr = true,
-    decr = false;
+console.log(personalMovieDB);
 
 
 
