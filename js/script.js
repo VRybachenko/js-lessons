7e2004f798088d608a1f1549246348014a08f9b1
vrybachenko <vrybache@softserveinc.com>
2023-06-16 14:19:32 +0300

    destructuring

--- js/script.js
+++ js/script.js
@@ -7,23 +7,33 @@ const options = {
     colors: {
         border: "black",
         bg: "red"
+    },
+    makeTest: function () {
+        console.log("Test!!");
     }
 }
 
-console.log(options.colors.border);
+options.makeTest();
 
-let counter = 0;
-for (let key in options) {
-    if(typeof (options[key]) === "object") {
-        for (let i in options[key]){
-            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-            counter ++;
-        }
-    } else {
-        console.log(`Свойство ${key} имеет значение ${options[key]}`);
-        counter ++;
-    }
-}
+//Деструктиризация
+const {border, bg} = options.colors;
+console.log(border)
+
+
+// console.log(options.colors.border);
+//
+// let counter = 0;
+// for (let key in options) {
+//     if(typeof (options[key]) === "object") {
+//         for (let i in options[key]){
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter ++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter ++;
+//     }
+// }
 
 console.log(Object.keys(options))
 console.log(Object.keys(options).length)
