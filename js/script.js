1e2f2a223293c1320054579c1c364391abbe0429
vrybachenko <rybachenko.vadim@gmail.com>
2024-06-24 20:02:57 +0300

    A cycle within a cycle and labels

--- js/script.js
+++ js/script.js
@@ -1,22 +1,44 @@
 "use strict";
 
-let num = 50;
-
-while (num <= 55) {
-    console.log(num);
-    num++
-}
-
-do {
-    console.log(num);
-    num++;
-} while (num < 55);
-
-for (let i = 1; i < 8; i++) {
-    if (i === 6) {
-        //break;
-        continue;
-    }
+for (let i = 0; i < 3; i++) {
     console.log(i);
-    num++
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
 }
+
+
+/*
+**
+***
+****
+*****
+******
+*/
+
+let result = "";
+const lenght = 7;
+
+for (let i = 1; i < lenght; i++) {
+    for (let j = 0; j < i; j++) {
+        result += "*";
+    }
+    result += "\n"
+}
+
+console.log(result);
+
+
+//LABEL in cycle
+first: for (let i = 1; i < 3; i++) {
+    console.log(`First level: ${i}`)
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if(k === 2) continue first;
+            console.log(`Third level: ${k}`)
+        }
+    }
+}
+
+
