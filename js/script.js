a520c27331c5231586690e9feb0c395094fbfe32
vrybachenko <rybachenko.vadim@gmail.com>
2024-07-18 20:08:51 +0300

    Practice, part 2. Applying conditions and loops

--- js/script.js
+++ js/script.js
@@ -1,44 +1,53 @@
-"use strict";
+/* Задание на урок:
 
-for (let i = 0; i < 3; i++) {
-    console.log(i);
-    for (let j = 0; j < 3; j++) {
-        console.log(j);
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит -
+возвращаем пользователя к вопросам опять. (К любой строке можно обратиться как
+str.length - и получить её длину)
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше -
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a !== '' && b !== '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
     }
 }
 
-
-/*
-**
-***
-****
-*****
-******
-*/
-
-let result = "";
-const lenght = 7;
-
-for (let i = 1; i < lenght; i++) {
-    for (let j = 0; j < i; j++) {
-        result += "*";
-    }
-    result += "\n"
-}
-
-console.log(result);
-
-
-//LABEL in cycle
-first: for (let i = 1; i < 3; i++) {
-    console.log(`First level: ${i}`)
-    for (let j = 0; j < 3; j++) {
-        console.log(`Second level: ${j}`);
-        for (let k = 0; k < 3; k++) {
-            if(k === 2) continue first;
-            console.log(`Third level: ${k}`)
-        }
-    }
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель")
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
 }
 
 
+console.log(personalMovieDB);
