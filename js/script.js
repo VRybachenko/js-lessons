535be0f0cc21988a1853f7943e85010cd3a06147
vrybachenko <rybachenko.vadim@gmail.com>
2024-09-08 19:33:03 +0300

    Pass by reference or by value, Spread operator (ES6-ES9)

--- js/script.js
+++ js/script.js
@@ -1,40 +1,97 @@
 'use strict';
 
-const arr = [2, 13, 26, 3, 6, 8, 10];
-arr.sort(compareNum);
-console.log(arr);
+let a = 5,
+    b = a;
 
-function compareNum(a, b) {
-    return a - b;
+b = b + 5;
+
+// console.log(a);
+// console.log(b);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+// const copy = obj; //Ссылку (передача по ссылки)
+// copy.a = b;
+//
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
 }
 
-// arr.pop(); // удалает последний метод массива
-// arr.push(10); // добавляет в конец массива число
-//
-// console.log(arr);
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
 
-arr.forEach(function (item, i, arr) {
-    console.log(`${i}: ${item} внутри массива ${arr}`);
+const newNumbers = copy(numbers);
 
-})
+newNumbers.a = 10;
+newNumbers.c.x = 10;
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
-//
-for(let value of arr){
-    console.log(value);
+// console.log(newNumbers);
+// console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+const clone = Object.assign({}, add);
+clone.d = 20;
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = "dasdasd";
+console.log(newArray);
+console.log(oldArray);
+
+const video = ["youtube", "vimeo", "rutube"],
+    blogs = ["wordprees", "livejournal", "blogger"],
+    internet = [...video, ...blogs, "instagram", "facebook"];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
-//
-// arr[99] = 0;
-//
-// console.log(arr.length);
-// console.log(arr);
 
-// const str = prompt("", "");
-// const products = str.split(", ");
-// products.sort();
-// console.log(products.join('; '));
+const num = [2, 5, 7];
+log(...num);
+
+const array = ["a", "b"];
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q};
+
+
+
+
+
 
 
 
