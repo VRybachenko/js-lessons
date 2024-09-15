ff25632fe28f26cb8c38596a2dc81b18b9be74a3
vrybachenko <rybachenko.vadim@gmail.com>
2024-09-15 19:38:31 +0300

    Tasks for working with objects and arrays

--- js/script.js
+++ js/script.js
@@ -1,95 +1,71 @@
 'use strict';
 
-let a = 5,
-    b = a;
+////////////////////////////////
+//Задачи на работу с объектами//
+////////////////////////////////
+const personalPlanPeter = {
+    name: "Peter",
+    age: "30",
+    skills: {
+        languages: ['ru', 'eng'],
+        programmingLangs: {
+            js: '20%',
+            php: '10%'
+        },
+        exp: '1 month'
+    },
+    showAgeAndLangs: function(plan) {
+        const { age, skills: { languages } } = plan;
+        const upperCaseLangs = languages.map(lang => lang.toUpperCase()).join(' ');
 
-b = b + 5;
-
-// console.log(a);
-// console.log(b);
-
-const obj = {
-    a: 5,
-    b: 1
-};
-
-// const copy = obj; //Ссылку (передача по ссылки)
-// copy.a = b;
-//
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj) {
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
+        return `Мне ${age} и я владею языками: ${upperCaseLangs}`;
     }
 };
 
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-// console.log(newNumbers);
-// console.log(numbers);
-
-const add = {
-    d: 17,
-    e: 20
-};
-
-const clone = Object.assign({}, add);
-clone.d = 20;
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = "dasdasd";
-console.log(newArray);
-console.log(oldArray);
-
-const video = ["youtube", "vimeo", "rutube"],
-    blogs = ["wordprees", "livejournal", "blogger"],
-    internet = [...video, ...blogs, "instagram", "facebook"];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
+function showExperience(plan) {
+    const {skills: {exp}} = plan;
+    return exp;
 }
 
-const num = [2, 5, 7];
-log(...num);
+function showProgrammingLangs(plan) {
+    let result = '';
+    const { programmingLangs } = plan.skills;
 
-const array = ["a", "b"];
-const newAarray = [...array];
+    for (let lang in programmingLangs) {
+        result += `Язык ${lang} изучен на ${programmingLangs[lang]}\n`;
+    }
 
-const q = {
-    one: 1,
-    two: 2
-};
+    return result.trim();
+}
 
-const newObj = {...q};
+console.log(showExperience(personalPlanPeter));
+console.log(showProgrammingLangs(personalPlanPeter));
+console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
 
+////////////////////////////////
+//Задачи на работу с массивами//
+////////////////////////////////
+const family = ['Peter', 'Ann', 'Alex', 'Linda'];
 
+function showFamily(arr) {
+    if (arr.length === 0) {
+        return 'Семья пуста';
+    } else {
+        return 'Семья состоит из: ' + arr.join(' ');
+    }
+}
 
+console.log(showFamily(family));
+
+const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+function standardizeStrings(arr) {
+    for (let city of arr) {
+        console.log(city.toLowerCase());
+    }
+}
+
+standardizeStrings(favoriteCities);
 
 
 
