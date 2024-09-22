c926764d7de59d63cbaf2c48f07b9583158908d8
vrybachenko <rybachenko.vadim@gmail.com>
2024-09-22 20:43:09 +0300

    OOP Basics, Prototype-Based Inheritance

--- js/script.js
+++ js/script.js
@@ -1,73 +1,34 @@
 'use strict';
 
-////////////////////////////////
-//Задачи на работу с объектами//
-////////////////////////////////
-const personalPlanPeter = {
-    name: "Peter",
-    age: "30",
-    skills: {
-        languages: ['ru', 'eng'],
-        programmingLangs: {
-            js: '20%',
-            php: '10%'
-        },
-        exp: '1 month'
-    },
-    showAgeAndLangs: function(plan) {
-        const { age, skills: { languages } } = plan;
-        const upperCaseLangs = languages.map(lang => lang.toUpperCase()).join(' ');
+let str = "some";
+let strObj = new String(str);
 
-        return `Мне ${age} и я владею языками: ${upperCaseLangs}`;
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+//console.dir([1,2,3,]);
+
+const soldier = {
+  health: 400,
+  armor: 100,
+    sayHello: function () {
+      console.log("Hello");
     }
 };
 
-function showExperience(plan) {
-    const {skills: {exp}} = plan;
-    return exp;
-}
+// const jonh = {
+//     health: 100
+// }
 
-function showProgrammingLangs(plan) {
-    let result = '';
-    const { programmingLangs } = plan.skills;
-
-    for (let lang in programmingLangs) {
-        result += `Язык ${lang} изучен на ${programmingLangs[lang]}\n`;
-    }
-
-    return result.trim();
-}
-
-console.log(showExperience(personalPlanPeter));
-console.log(showProgrammingLangs(personalPlanPeter));
-console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
-
-////////////////////////////////
-//Задачи на работу с массивами//
-////////////////////////////////
-const family = ['Peter', 'Ann', 'Alex', 'Linda'];
-
-function showFamily(arr) {
-    if (arr.length === 0) {
-        return 'Семья пуста';
-    } else {
-        return 'Семья состоит из: ' + arr.join(' ');
-    }
-}
-
-console.log(showFamily(family));
-
-const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
-
-function standardizeStrings(arr) {
-    for (let city of arr) {
-        console.log(city.toLowerCase());
-    }
-}
-
-standardizeStrings(favoriteCities);
+const jonh = Object.create(soldier);
 
 
+//Old variant(deprecated)
+// jonh.__proto__ = soldier;
 
+Object.setPrototypeOf(jonh, soldier);
+
+// console.log(jonh.armor);
+jonh.sayHello();
 
 
