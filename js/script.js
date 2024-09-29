4c7a9483f5f1b84de7d07edbcaed14de1f5d8acc
vrybachenko <rybachenko.vadim@gmail.com>
2024-09-29 20:38:51 +0300

    Closure and lexical environment

--- js/script.js
+++ js/script.js
@@ -1,49 +1,50 @@
 'use strict';
 
-//To string
-// 1)
-console.log(typeof (String(null)));
-console.log(typeof (String(4)));
+// let number = 5; debugger
+//
+// function logNumber() {
+//     console.log(number); debugger
+// }
+//
+// number = 6;
+// logNumber(); debugger
+//
+// number = 8;
+// logNumber(); debugger
 
-// 2)
-console.log(typeof (4 + ''));
+function createCounter() {
+    let counter = 0;
 
-const num = 5;
-console.log("https://vk.com/catalog/" + num);
+    const myFunction = function () { debugger
+        counter = counter + 1; debugger
+        return counter; debugger
+    }
 
-const fontSize = 26 + 'px';
-
-// To Number
-// 1)
-console.log(typeof (Number("4")));
-
-// 2)
-console.log(typeof (+'5')); //унарний плюс
-
-// 3)
-console.log(typeof(parseInt("15px", 10)));
-
-//let answer = +prompt("Hello", "");
-
-// To boolean
-
-//0, '', null, undefined, NaN - always FALSE!
-
-// 1)
-let switcher = null;
-
-if(switcher) {
-    console.log("Working...");
+    return myFunction;
 }
 
-switcher = 1;
+debugger
+const increment = createCounter();
+const c1 = increment(); debugger //1
+const c2 = increment(); debugger //2
+const c3 = increment(); debugger //3
 
-if(switcher) {
-    console.log("Working...");
+console.log(c1, c2, c3);
+
+{
+    let msg = "Hello";
 }
 
-// 2)
-console.log(typeof (Boolean("4")));
+console.log(msg); //Error!
 
-// 3)
-console.log(typeof (!!"4")); //очень редкий кейс, практически не юзається
+
+for(let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+        let num = 3;
+    }
+
+    console.log(num); //Error!
+}
+
+
+
