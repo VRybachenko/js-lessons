7e8a7ba8c5839c9dbc37573fa8b4a6219565027b
vrybachenko <rybachenko.vadim@gmail.com>
2024-10-25 21:52:57 +0300

    Getting elements from a page

--- js/script.js
+++ js/script.js
@@ -1,55 +1,20 @@
 'use strict';
 
-const restorantData = {
-    menu: [
-        {
-            name: 'Salad Caesar',
-            price: '14$'
-        },
-        {
-            name: 'Pizza Diavola',
-            price: '9$'
-        },
-        {
-            name: 'Beefsteak',
-            price: '17$'
-        },
-        {
-            name: 'Napoleon',
-            price: '7$'
-        }
-    ],
-    waitors: [
-        {name: 'Alice', age: 22}, {name: 'John', age: 24}
-    ],
-    averageLunchPrice: '20$',
-    openNow: true
-};
+const box = document.getElementById("box");
+console.log(box);
 
-function isOpen(prop) {
-    return prop ? 'Открыто' : 'Закрыто';
-}
+const btns = document.getElementsByTagName('button');
+console.log(btns[1]);
 
-console.log(isOpen(restorantData.openNow))
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-function isAverageLunchPriceTrue(fDish, sDish, average) {
-    const price1 = +fDish.price.slice(0, -1);
-    const price2 = +sDish.price.slice(0, -1);
-    average = +average.slice(0, -1);
-    if (price1 + price2 < average) {
-        return 'Цена ниже средней';
-    } else {
-        return 'Цена выше средней';
-    }
-}
+const hearts = document.querySelectorAll('.heart');
+console.log(hearts);
 
-console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+hearts.forEach(item => {
+    console.log(item);
+});
 
-function transferWaitors(data) {
-    const copy = JSON.parse(JSON.stringify(data));
-
-    copy.waitors[0] = {name: 'Mike', age: 32};
-    return copy;
-}
-
-transferWaitors(restorantData);
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
