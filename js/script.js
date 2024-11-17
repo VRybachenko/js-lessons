c9a331e38962de6b82c307d9575cfff1ac6c4a09
vrybachenko <rybachenko.vadim@gmail.com>
2024-11-17 18:31:07 +0200

    Recursion

--- js/script.js
+++ js/script.js
@@ -1,22 +1,100 @@
 'use strict';
 
-// console.log(document.body);
-// console.log(document.documentElement);
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(document.body.firstElementChild);
-// console.log(document.body.lastChild);
+// function pow(x, n) {
+//     let result = 1;
+//
+//     for (let i = 1; i < n; i++) {
+//         result *= x;
+//     }
+//
+//     return result;
+// }
 
-// console.log(document.querySelector('#current').parentNode.parentNode);
-// console.log(document.querySelector('#current').parentElement);
-// console.log(document.querySelector('[data-current="3"]').previousSibling);
+// function pow(x, n) {
+//     if (n === 1) {
+//         return x;
+//     } else {
+//         return x * pow(x, n - 1)
+//     }
+// }
 
-// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+Math.pow(2, 2)
 
-for(let node of document.body.childNodes) {
-    if(node.nodeName == '#text') {
-        continue;
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+            name: 'Ann',
+            progress: 18
+        }],
+        pro: [{
+            name: 'Sam',
+            progress: 10
+        }],
+        semi: {
+            students: [{
+                name: 'Test',
+                progress: 100
+            }]
+        }
     }
-    console.log(node);
+};
+
+function getTotalProgressByIteration(data) {
+    let total = 0;
+    let students = 0;
+
+    for (let course of Object.values(data)) {
+        if (Array.isArray(course)) {
+            students += course.length;
+
+            for (let i = 0; i < course.length; i++) {
+                total += course[i].progress;
+            }
+        } else {
+            for (let subCourse of Object.values(course)) {
+                students += subCourse.length;
+                for (let i = 0; i < subCourse.length; i++) {
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
+
+    return total / students;
 }
 
+//console.log(getTotalProgressByIteration(students));
+
+
+function getTotalProgressByRecursion(data) {
+    if (Array.isArray(data)) {
+        let total = 0;
+
+        for (let i = 0; i < data.length; i++) {
+            total += data[i].progress;
+        }
+
+        return [total, data.length];
+    } else {
+        let total = [0, 0];
+        for (let subData of Object.values(data)) {
+            const sabDataArr = getTotalProgressByRecursion(subData);
+            total[0] += sabDataArr[0];
+            total[1] += sabDataArr[1];
+        }
+        return total;
+    }
+}
+
+const result = getTotalProgressByRecursion(students);
+console.log(result[0] / result[1]);
