4370a8d3c4706609cc14f6c693a208daea3477cd
vrybachenko <rybachenko.vadim@gmail.com>
2024-12-22 19:43:09 +0200

    Async, defer, dynamic scripts

--- js/script.js
+++ js/script.js
@@ -1,37 +1,13 @@
 'use strict';
 
-// touchstart - спрацьовує, коли палець торкається елемента.
-// touchmove - спрацьовує, коли палець рухається по екрану (після touchstart).
-// touchend - спрацьовує, коли палець піднімається (торкання завершується).
-// touchenter - спрацьовує, коли палець заходить на межі елемента.
-// touchleave - спрацьовує, коли палець виходить за межі елемента.
-// touchcancel - спрацьовує, коли подія торкання скасовується (наприклад, через системне переривання).
+const p = document.querySelectorAll('p');
+console.log(p);
 
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
 
-window.addEventListener('DOMContentLoaded', () => {
-    const box = document.querySelector('.box');
-
-    box.addEventListener('touchstart', (e) => {
-        e.preventDefault();
-
-        console.log('Start');
-        console.log(e.targetTouches)
-    })
-
-    box.addEventListener('touchmove', (e) => {
-        e.preventDefault();
-
-        //console.log('Move');
-        console.log(e.targetTouches[0].pageX); // Виводить координату X першого дотику відносно сторінки.
-    })
-
-    box.addEventListener('touchend', (e) => {
-        e.preventDefault();
-
-        console.log('End');
-    })
-});
-
-// touches - масив усіх пальців, що наразі торкаються екрана.
-// targetTouches - масив пальців, що торкаються конкретного елемента (на якому спрацювала подія).
-// changedTouches - масив пальців, які змінили свій стан під час події (наприклад, додано або прибрано дотик).
+loadScript("js/test.js");
