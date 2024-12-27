869a484d9540b24b41582dcc0c96b0c02f989594
vrybachenko <rybachenko.vadim@gmail.com>
2024-12-27 20:27:29 +0200

    Optional Chaining Operator (?.) ES11

--- js/script.js
+++ js/script.js
@@ -1,18 +1,32 @@
 'use strict';
 
 const box = document.querySelector('.box');
+const block = document.querySelector('.block');
 
-const newHeight = 100;
-const newWidth = 400;
+console.log(block);
 
-function changeParams(elem, h, w) {
-    elem.style.height = `${h ?? 200}px`;
-    elem.style.width = `${w ?? 200}px`;
-    elem.innerHTML = (h ?? 200) * (w ?? 200);
+// if (block) {
+//     console.log(block.textContent);
+// }
+
+console.log(block?.textContent);
+
+console.log(1 + 2);
+
+const userData = {
+    name: 'Ivan',
+    age: null,
+    say: function () {
+        console.log("Hello!")
+    }
 }
 
-changeParams(box, newHeight, newWidth);
+userData.say();
+userData.hey?.();
 
-let userName;
-let userKey;
-console.log(userName && userKey ?? 'User');
+
+// if(userData && userData.skills && userData.skills.js) {
+//     console.log(userData.skills.js)
+// }
+
+console.log(userData?.skills?.js)
