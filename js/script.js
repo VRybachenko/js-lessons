22aa628b6127b2de648006e557b9765275916b2c
vrybachenko <rybachenko.vadim@gmail.com>
2025-03-15 21:42:03 +0200

    Scripts and their execution time. setTimeout and setInterval

--- js/script.js
+++ js/script.js
@@ -1,39 +1,41 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items');
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
 
-    function hideTapContent() {
-        tabsContent.forEach(item => {
-            item.style.display = 'none';
-        });
+function myAnimation() {
+    const elem = document.querySelector('.box');
+    let pos = 0;
 
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        })
-    }
-
-    function showTapContent(i = 0) {
-        tabsContent[i].style.display = 'block';
-        tabs[i].classList.add('tabheader__item_active');
-    }
-
-    hideTapContent();
-    showTapContent();
-
-    tabsParent.addEventListener('click', (event) => {
-        const target = event.target;
-
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTapContent();
-                    showTapContent(i);
-                }
-            });
+    const id = setInterval(frame, 10);
+    function frame() {
+        if(pos == 300) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + "px";
+            elem.style.left = pos + 'px';
         }
-    });
-});
+    }
+}
 
+btn.addEventListener('click', myAnimation);
+
+// btn.addEventListener('click', () => {
+//     // const timerId = setTimeout(logger, 2000);
+//     timerId = setInterval(logger, 500);
+// });
+//
+// function logger() {
+//     if(i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('text');
+//     i++;
+// }
+
+// let id = setTimeout(function log(){
+//     console.log("Hello");
+//     id = setTimeout(log, 500);
+// }, 500);
