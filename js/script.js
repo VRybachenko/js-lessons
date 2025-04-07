324cfd553daf09139275fcb58b18cf6f96a45b41
vrybachenko <rybachenko.vadim@gmail.com>
2025-04-07 22:29:00 +0300

    Working with dates

--- js/script.js
+++ js/script.js
@@ -1,63 +1,30 @@
 'use strict';
 
-// let user = {name: "Vadym"};
+const now = new Date("2025-04-07");
+Date.parse("2025-04-07");
 
-//const arr = [user];
-// let map = new Map();
-// let map = new WeakMap();
-// map.set(user, "data");
-//
-// user = null;
-
-// console.log(user);
-//console.log(arr[0]);
-// console.log(map.keys());
-// console.log(map.has(user));
-// console.log(map);
+console.log(now.setHours(40));
+console.log(now);
 
 /*
-let cache = new WeakMap();
+console.log(now.getFullYear());
+console.log(now.getMonth());
+console.log(now.getDate());
+console.log(now.getDay());
+console.log(now.getUTCHours());
+*/
 
-function cashUser(user) {
-    if(!cache.has(user)) {
-        cache.set(user, Date.now());
-    }
-    return cache.get(user);
+// console.log(now.getTimezoneOffset())
+// console.log(now.getTime())
+
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++) {
+    let some = i ** 3;
 }
 
-let lena = {name: "Elena"};
-let alex = {name: "Alex"};
+let end = new Date();
 
-cashUser(lena);
-cashUser(alex);
-
-lena = null;
-
-console.log(cache.has(lena));
-console.log(cache.has(alex));
- */
-
-
-//WeekSet
-//add, has, delete
-
-let message = [
-    {text: "Hello", from: "John"},
-    {text: "Hello World", from: "Mike"},
-    {text: "Hello Bob", from: "Aragorn"},
-    {text: "Hello Frodo", from: "Gendalf"}
-];
-
-let readMessages = new WeakSet();
-
-readMessages.add(message[0]);
-// readMessages.add(message[1]);
-
-readMessages.add(message[0]);
-message.shift();
-console.log(readMessages.has(message[0]));
-
-
-
-
+alert(`Цикл отработал за ${end - start} миллисекунд`);
 
