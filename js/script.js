5c7612c1bd83a3a7c15b6db148d05e6710a5ca93
vrybachenko <rybachenko.vadim@gmail.com>
2025-06-11 20:57:04 +0300

    Constructor functions

--- js/script.js
+++ js/script.js
@@ -1,149 +1,26 @@
 'use strict';
 
-window.addEventListener('DOMContentLoaded', () => {
-    //Tabs
-    const tabs = document.querySelectorAll('.tabheader__item'),
-        tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items');
-
-    function hideTapContent() {
-        tabsContent.forEach(item => {
-            item.style.display = 'none';
-        });
-
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        })
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function (){
+        console.log(`Hello ${this.name}!`);
     }
+}
 
-    function showTapContent(i = 0) {
-        tabsContent[i].style.display = 'block';
-        tabs[i].classList.add('tabheader__item_active');
-    }
+User.prototype.exit = function () {
+    console.log(`User exit: ${this.name}`);
+}
 
-    hideTapContent();
-    showTapContent();
+const vadym = new User("Vadym", 31);
+const alex = new User("Alex", 20);
 
-    tabsParent.addEventListener('click', (event) => {
-        const target = event.target;
+vadym.exit();
+alex.exit();
 
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTapContent();
-                    showTapContent(i);
-                }
-            });
-        }
-    });
+vadym.hello();
+alex.hello();
 
-    //Timer
-    const deadline = '2025-05-10';
-
-    function getTimeRemaining(endtime) {
-        let days, hours, minutes, seconds;
-        const t = Date.parse(endtime) - Date.now();
-        if (t <= 0) {
-            return {
-                days: 0,
-                hours: 0,
-                minutes: 0,
-                seconds: 0
-            }
-        } else {
-            days = Math.floor(t / (1000 * 60 * 60 * 24));
-            hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-            minutes = Math.floor((t / 1000 / 60) % 60);
-            seconds = Math.floor((t / 1000) % 60);
-        }
-
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        }
-    }
-
-    function getZero(num) {
-        if (num >= 0 && num < 10) {
-            return `0${num}`;
-        } else {
-            return num;
-        }
-    }
-
-    function setClock(selector, endtime) {
-        const timer = document.querySelector(selector),
-            days = timer.querySelector('#days'),
-            hours = timer.querySelector('#hours'),
-            minutes = timer.querySelector('#minutes'),
-            seconds = timer.querySelector('#seconds'),
-            timeinterval = setInterval(updateClock, 1000);
-
-        updateClock();
-
-        function updateClock() {
-            const t = getTimeRemaining(endtime);
-
-            days.innerHTML = getZero(t.days);
-            hours.innerHTML = getZero(t.hours);
-            minutes.innerHTML = getZero(t.minutes);
-            seconds.innerHTML = getZero(t.seconds);
-
-            if (t.total <= 0) {
-                clearInterval(timeinterval);
-            }
-        }
-    }
-
-    setClock('.timer', deadline);
-
-    //Modal
-    const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-close]');
-
-    function openModal() {
-        modal.classList.add('show');
-        modal.classList.remove('hide');
-        document.body.style.overflow = 'hidden';
-        clearInterval(modalTimerId);
-    }
-
-    modalTrigger.forEach(btn => {
-        btn.addEventListener('click', openModal);
-    });
-
-    function closeModal() {
-        modal.classList.add('hide');
-        modal.classList.remove('show');
-        document.body.style.overflow = '';
-    }
-
-    modalCloseBtn.addEventListener('click', closeModal);
-
-    modal.addEventListener('click', (e) => {
-        if (e.target === modal) {
-            closeModal();
-        }
-    })
-
-    document.addEventListener('keydown', (e) => {
-        if (e.code === 'Escape' && modal.classList.contains('show')) {
-            closeModal();
-        }
-    });
-
-    const modalTimerId = setTimeout(openModal, 5000);
-
-    function showModalByScroll() {
-        if (window.scrollY + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
-            openModal();
-            window.removeEventListener('scroll', showModalByScroll);
-        }
-    }
-
-    window.addEventListener('scroll', showModalByScroll);
-});
+console.log(vadym);
+console.log(alex);
